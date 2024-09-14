f1ae43c1ad197e7b8ef33c05e70ac07f171a1996
Eliseo <eli.feuli37@gmail.com>
2024-09-13 23:32:32 -0300

    Ejercicio7

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -14,17 +14,20 @@ type State = M.Map Variable Int
 -- Estado vacío
 -- Completar la definición
 initState :: State
-initState = undefined
+initState = M.empty
 
 -- Busca el valor de una variable en un estado
 -- Completar la definición
 lookfor :: Variable -> State -> Int
-lookfor = undefined
+lookfor w [] = 0
+lookfor w ((x, n) : s) | (w == x) = n
+                       | otherwise = lookfor w s
 
 -- Cambia el valor de una variable en un estado
 -- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update w n ((x, y) : s) =  | (w == x) = (x, n) : s
+                           | otherwise = (x, y) : (update w n s)
 
 -- Evalúa un programa en el estado vacío
 eval :: Comm -> State
@@ -39,9 +42,51 @@ stepCommStar c    s = Data.Strict.Tuple.uncurry stepCommStar $ stepComm c s
 -- Evalúa un paso de un comando en un estado dado
 -- Completar la definición
 stepComm :: Comm -> State -> Pair Comm State
-stepComm = undefined
+stepComm Skip s          = (Skip !:! s)  
+stepComm (Let x n) s     = (Skip !:! update x n s)
+stepComm (Seq Skip c) s  = (c2 !:! s) 
+stepComm (Seq c0 c1) s   = ((Seq c0' c1) !:! s')
+                        where
+                          (c0',s') = stepComm c0 s
+stepComm (IfThenElse b c0 c1) s = case evalExp b s of
+                                      (BTrue, s')  -> (c0 !:! s')
+                                      (BFalse, s') -> (c1 !:! s')
+stepComm RepeatUntil c b = (Seq c (IfThenElse b Skip (RepeatUntil c b)) !:! s)
 
 -- Evalúa una expresión
 -- Completar la definición
 evalExp :: Exp a -> State -> Pair a State
-evalExp = undefined
+evalExp (Const n) s = (n !:! s)
+evalExp (Var x) s = (lookfor x s !:! s)
+evalExp (VarInc x) s = (x+1 !:! update (x+1) s)
+evalExp (VarDec x) s = (x-1 !:! update (x-1) s)
+evalExp (UMinus e) s = (-n !:! s')
+                    where 
+                      (n !:! s') = evalExp e s
+evalExp expr s = let (n0 !:! s')  = evalExp e0 s
+                     (n1 !:! s'') = evalExp e1 s'
+                 in case expr of
+                     Plus e0 e1  -> (n0 + n1 !:! s'')
+                     Minus e0 e1 -> (n0 - n1 !:! s'')
+                     Times e0 e1 -> (n0 * n1 !:! s'')
+                     Div e0 e1   -> (n0 `div` n1 !:! s'')
+evalExp BTrue s = (True !:! s)
+evalExp BFalse s = (False !:! s)
+evalExp (Not p) s = case evalExp p s of
+                         (True, s')  -> (False !:! s')
+                         (False, s') -> (True !:! s')
+evalExp expr = let (b0 !:! s')  = evalExp p1 s
+                   (b1 !:! s'') = evalExp p0 s'
+                 in case expr of
+                         And p0 p1 ->  (b0 && b1 !:! s'')  
+                         Or p0 p1  ->  (b0 || b1 !:! s'')
+evalExp expr s = let (n0' !:! s')  = evalExp e0 s
+                     (n1' !:! s'') = evalExp e1 s'
+                 in case expr of
+                         Lt e0 e1  -> (n0' < n1' !:! s'')
+                         Gt e0 e1  -> (n0' > n1' !:! s'')
+                         Eq e0 e1  -> (n0' == n1' !:! s'')
+                         NEq e0 e1 -> (n0' != n1' !:! s'')
+
+
+

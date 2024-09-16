996df1ac929f5d9153d12d3dcb5d4d8d5786e2cd
agussamper <agustinsamper3@gmail.com>
2024-09-15 21:09:35 -0300

    indentacion

--- src/Eval3.hs
+++ src/Eval3.hs
@@ -45,20 +45,20 @@ stepCommStar c    s = do
 -- Completar la definición
 stepComm :: Comm -> State -> Either Error (Pair Comm State)
 stepComm Skip s          = Right (Skip :!: s)  
-stepComm (Let x e) s     =  do 
-                      (n :!: s') <- evalExp e s 
-                      return (Skip :!: M.insert x n s')
+stepComm (Let x e) s     =  
+  do (n :!: s') <- evalExp e s 
+     return (Skip :!: M.insert x n s')
 stepComm (Seq Skip c) s  = Right (c :!: s) 
-stepComm (Seq c0 c1) s   = do 
-                        (c0' :!: s') <- stepComm c0 s
-                        return ((Seq c0' c1) :!: s')
-stepComm (IfThenElse b c0 c1) s = do
-                                (b' :!: s') <- evalExp b s 
-                                if b' then return (c0 :!: s')
-                                else return (c1 :!: s')
-stepComm (RepeatUntil c b) s = do
-                            let c1 = IfThenElse b Skip (RepeatUntil c b)
-                            return ((Seq c c1) :!: s)
+stepComm (Seq c0 c1) s   = 
+  do (c0' :!: s') <- stepComm c0 s
+     return (Seq c0' c1 :!: s')
+stepComm (IfThenElse b c0 c1) s = 
+  do (b' :!: s') <- evalExp b s 
+     if b' then return (c0 :!: s')
+     else return (c1 :!: s')
+stepComm (RepeatUntil c b) s =
+  do let c1 = IfThenElse b Skip (RepeatUntil c b)
+     return (Seq c c1 :!: s)
 
 -- Evalúa una expresión
 -- Completar la definición
@@ -83,44 +83,34 @@ evalExp (Eq e0 e1)  s = evalExpBool (Eq e0 e1)  s
 evalExp (NEq e0 e1) s = evalExpBool (NEq e0 e1) s
 
 
-
 evalExpInt :: Exp Int -> State -> Either Error (Pair Int State)
 evalExpInt (Const n)  s = Right (n :!: s)
-evalExpInt (Var x)    s = do
-                      x' <- lookfor x s 
-                      return (x' :!: s)
-evalExpInt (VarInc x) s =  do 
-                           n <- lookfor x s
-                           let s' = update x (n + 1) s 
-                           return ((n+1) :!: s')
-evalExpInt (VarDec x) s = do 
-                           n <- lookfor x s
-                           let s' = update x (n - 1) s 
-                           return ((n-1) :!: s')
-evalExpInt (UMinus e) s = do
-                      (n :!: s') <- evalExp e s
-                      return (-n :!: s')
-evalExpInt (Plus  e0 e1) s = do 
-                            n <- evalOpp (+)   e0 e1 s
-                            return n
-evalExpInt (Minus e0 e1) s = do 
-                            n <- evalOpp (-)   e0 e1 s
-                            return n
-evalExpInt (Times e0 e1) s = do 
-                            n <- evalOpp (*)   e0 e1 s
-                            return n
-evalExpInt (Div   e0 e1) s = do 
-                            n <- evalOpp (div) e0 e1 s
-                            return n
+evalExpInt (Var x)    s = 
+  do x' <- lookfor x s 
+     return (x' :!: s)
+evalExpInt (VarInc x) s =  
+  do n <- lookfor x s
+     let s' = update x (n + 1) s 
+     return ((n+1) :!: s')
+evalExpInt (VarDec x) s = 
+  do n <- lookfor x s
+     let s' = update x (n - 1) s 
+     return ((n-1) :!: s')
+evalExpInt (UMinus e) s = 
+  do (n :!: s') <- evalExp e s
+     return (-n :!: s')
+evalExpInt expr s = do evalOpp expr s
+
 
 evalExpBool::Exp Bool -> State -> Either Error (Pair Bool State)
 evalExpBool BTrue  s = Right (True  :!: s)
 evalExpBool BFalse s = Right (False :!: s)
-evalExpBool (Not p) s = case evalExpBool p s of
-                         Left DivByZero       -> Left DivByZero
-                         Left UndefVar        -> Left UndefVar
-                         Right (True :!: s')  -> Right (False :!: s')
-                         Right (False :!: s') -> Right (True :!: s')
+evalExpBool (Not p) s = 
+  case evalExpBool p s of
+    Left DivByZero       -> Left DivByZero
+    Left UndefVar        -> Left UndefVar
+    Right (True :!: s')  -> Right (False :!: s')
+    Right (False :!: s') -> Right (True :!: s')
 evalExpBool (And p0 p1) s = evalLog (&&) p0 p1 s
 evalExpBool (Or p0 p1)  s = evalLog (||) p0 p1 s
 evalExpBool (Lt e0 e1)  s = evalComp (<)  e0 e1  s
@@ -129,27 +119,35 @@ evalExpBool (Eq e0 e1)  s = evalComp (==) e0 e1 s
 evalExpBool (NEq e0 e1) s = evalComp (/=) e0 e1 s
 
 
-evalOpp :: (Int -> Int -> Int) -> Exp Int -> Exp Int -> State -> Either Error (Pair Int State)
-evalOpp div e0 e1 s = do
-                    (n0 :!: s')  <- evalExpInt e0 s
-                    (n1 :!: s'') <- evalExpInt e1 s'
-                    if n1 == 0 then (Left DivByZero)
-                    else return (div n0 n1 :!: s'')
-evalOpp op e0 e1 s = do
-                  (n0 :!: s') <- evalExpInt e0 s
-                  (n1 :!: s'') <- evalExpInt e1 s'
-                  return (op n0 n1 :!: s'')
+evalOpp :: Exp Int -> State -> Either Error (Pair Int State)
+evalOpp (Div x y) s = 
+  do
+    (n0 :!: s')  <- evalExpInt x s
+    (n1 :!: s'') <- evalExpInt y s'
+    if n1 == 0 then Left DivByZero
+    else return (div n0 n1 :!: s'')
+evalOpp expri s =
+  let (op, x, y) = getOp expri in
+  do
+    (n0 :!: s') <- evalExpInt x s
+    (n1 :!: s'') <- evalExpInt y s'
+    return (op n0 n1 :!: s'')
+
+getOp :: Exp Int -> (Int -> Int -> Int,Exp Int,Exp Int)
+getOp (Times x y) =  ((*),x,y)
+getOp (Minus x y) = ((-),x,y)
+getOp (Plus x y) = ((+),x,y)
 
 
 evalLog :: (Bool -> Bool -> Bool) -> Exp Bool -> Exp Bool -> State -> Either Error (Pair Bool State)
-evalLog op p0 p1 s = do 
-                  (n0 :!: s')  <- evalExpBool p0 s
-                  (n1 :!: s'') <- evalExpBool p1 s'
-                  return (op n0 n1 :!: s'')
+evalLog op p0 p1 s = 
+  do (n0 :!: s')  <- evalExpBool p0 s
+     (n1 :!: s'') <- evalExpBool p1 s'
+     return (op n0 n1 :!: s'')
 
 
 evalComp :: (Int -> Int -> Bool) -> Exp Int -> Exp Int -> State -> Either Error (Pair Bool State)
-evalComp op e0 e1 s = do 
-                    (n0 :!: s')  <- evalExpInt e0 s
-                    (n1 :!: s'') <- evalExpInt e1 s'
-                    return (op n0 n1 :!: s'')
+evalComp op e0 e1 s = 
+  do (n0 :!: s')  <- evalExpInt e0 s
+     (n1 :!: s'') <- evalExpInt e1 s'
+     return (op n0 n1 :!: s'')
